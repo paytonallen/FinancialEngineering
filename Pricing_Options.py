581f1b3738376ee9b3d61dbdc03709f7b05c68e5
paytonallen <paytonkallen11@icloud.com>
2022-08-19 00:19:21 -0500

    Black Scholes - Trinomial Trees- Binomial Trees

--- Pricing_Options.py
+++ Pricing_Options.py
@@ -1,12 +1,107 @@
 import math
 from turtle import pd
 import pandas as pd
-import numpy as np
+import numpy as np 
 from decimal import Decimal
 from abc import ABC, abstractmethod
 import scipy.linalg as linalg
 import sys
+from scipy.stats import norm
+from py_vollib.black_scholes import black_scholes as bs
+from py_vollib.black_scholes.greeks.analytical import delta,gamma,vega,theta,rho
 
+
+
+
+def BlackScholesModel(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the price of an option using Black Scholes Model
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    try:
+        if type == 'c':
+            price = S*norm.cdf(d1,0,1)-K*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == 'p':
+            price = K*np.exp(-r*T)*norm.cdf(-d2,0,1)- S*norm.cdf(-d1,0,1)
+        return price, bs(type,S,K,T,r,sigma)
+
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')
+
+def delta_calc(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the delta of an option
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    try:
+        if type == 'c':
+            delta_calc = norm.cdf(d1,0,1)
+        elif type == 'p':
+            delta_calc = -norm.cdf(-d1,0,1)
+        return delta_calc, delta(type, S,K,T,r,sigma)
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')  
+
+def gamma(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the gamma of an option using Black Scholes Model
+    normal distribution
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    try:
+        gamma_calc = norm.cdf(d1,0,1)/(S*sigma*np.sqrt(T))
+        return gamma_calc, gamma(type,S,K,T,r,sigma)
+
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')
+
+def vega_calc(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the vega of an option using Black Scholes Model
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    try:
+        vega_calc = S*norm.cdf(d1,0,1)*np.sqrt(T)
+        return vega_calc*0.01, vega(type,S,K,T,r,sigma)
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')
+
+
+def theta_calc(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the price of an option using Black Scholes Model
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    try:
+        if type == 'c':
+            theta_calc = -S*norm.cdf(d1,0,1)*sigma/(2*np.sqrt(T)) - r*K*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == 'p':
+            theta_calc = -S*norm.cdf(d1,0,1)*sigma/(2*np.sqrt(T)) + r*K*np.exp(-r*T)*norm.cdf(-d2,0,1)
+        return theta_calc/365, theta(type,S,K,T,r,sigma)
+
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')
+
+
+def rho_calc(r,S,K,T,sigma,type='c'):
+    """
+    Calculating the price of an option using Black Scholes Model
+    """
+    d1 = (np.log(S/K)+(r+sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    try:
+        if type == 'c':
+            rho_calc = K*T*np.exp(-r*T)*norm.cdf(d2,0,1)
+        elif type == 'p':
+            rho_calc = -K*T*np.exp(-r*T)*norm.cdf(-d2,0,1)
+        return rho_calc*0.01, rho(type,S,K,T,r,sigma)
+
+    except:
+        print('Please confirm option type, either "c" for Call or "p" for Put!')
 """
 class will store factors included stock option
 """
